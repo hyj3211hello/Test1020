399aa8ef4b2928c147712fe81fd800c4459c6a00
Administrator <1282987451@qq.com>
2016-10-20 17:36:27 +0800

    MainActivity里面定义了控件

--- app/src/main/java/qianfeng/com/myapplication/MainActivity.java
+++ app/src/main/java/qianfeng/com/myapplication/MainActivity.java
@@ -2,9 +2,12 @@ package qianfeng.com.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
