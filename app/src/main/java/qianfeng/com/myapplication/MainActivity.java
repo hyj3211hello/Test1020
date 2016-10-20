076c85ea26737b4dd64914f8cb43735f4779b2f2
SONE <847187384@qq.com>
2016-10-20 18:31:35 +0800

    第3次,aoxiang增加了控件

--- app/src/main/java/qianfeng/com/myapplication/MainActivity.java
+++ app/src/main/java/qianfeng/com/myapplication/MainActivity.java
@@ -2,9 +2,13 @@ package qianfeng.com.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
